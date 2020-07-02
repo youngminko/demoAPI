bc85ef7a591b8832c39738ccdd703a96746b8849
Young Ko <youngko@Youngs-Air.lan>
2020-07-02 16:27:46 -0400

    gitignore fixed

--- app.js
+++ app.js
@@ -71,7 +71,51 @@ app.route("/articles")
           res.send("No matching articles found." + foundArticle);
         }
       })
+    })
+
+    .put(function(req, res){
+
+      Article.update(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {overwrite: true},
+        function(err){
+          if(!err){
+            res.send("Successfully updated the selected article.");
+          }
+        }
+      );
+    })
+
+    .patch(function(req, res){
+
+      Article.update(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+          if(!err){
+            res.send("Successfully updated article.");
+          } else {
+            res.send(err);
+          }
+        }
+      );
+    })
+
+    .delete(function(req, res){
+
+      Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err){
+          if (!err){
+            res.send("Successfully deleted the corresponding article.");
+          } else {
+            res.send(err);
+          }
+        }
+      );
     });
+
 
 
 app.listen(3000, function() {
